f28e2f39f0f0f67fee217f645ecc89f6d4517e5c
JohanTv <40219652+JohanTv@users.noreply.github.com>
2019-08-25 23:36:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,30 @@
 using namespace std;
 
 int main() {
-	return EXIT_SUCCESS;
-}
+    int cases;
+    int walls;
 
+    int posAntes;
+    int posDespues;
+
+    int saltosAltos=0;
+    int saltosBajos=0;
+
+    cin>>cases;
+
+    for( int i=0 ; i<cases ; i++){
+        cin>>walls;
+        cin>>posAntes;
+        for( int j=1 ; j<walls ; j++ ){
+            cin>>posDespues;
+            if (posAntes<posDespues) saltosAltos++;
+            else if (posAntes>posDespues) saltosBajos++;
+            posAntes = posDespues;
+        }
+        cout<<"Case "<<i+1<<": "<<saltosAltos<<" "<<saltosBajos<<endl;
+        saltosAltos=0;
+        saltosBajos=0;
+    }
+
+    return 0;
+}
